7caa2eff52172223d5b77c485d153c3c371e19ab
ebicomp <ebicomp@gmail.com>
2023-01-03 00:15:21 +0330

    Add directives

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -7,6 +7,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent {
+
+onAddIngredient($event: Ingredient) {
+  this.ingredients.push($event);
+}
 ingredients :Ingredient[]= [
   new Ingredient('Apples' , 5),
   new Ingredient('Tomatoes' , 3),
